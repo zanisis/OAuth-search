8d6a3118ad06534cc34a04eca38dbc721c3d602b
zanisis <adzannyaa@gmail.com>
2017-04-27 12:56:59 +0700

    MVC OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const OAuth = require('oauth');
-require('dotenv').config();
+// const OAuth = require('oauth');
+const controller = require('../controller/twatscontroller');
+// require('dotenv').config();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var oauth = new OAuth.OAuth(
-        'https://api.twitter.com/oauth/request_token',
-        'https://api.twitter.com/oauth/access_token',
-        process.env.CONSUMER_KEY, //your application consumer key
-        process.env.CONSUMER_SECRET, //your application secret
-        '1.0A',
-        null, //this is callback
-        'HMAC-SHA1'
-      );
-      oauth.get(
-      'https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi',
-      process.env.TOKEN, //test user token
-      process.env.TOKEN_SECRET, //test user secret
-      function (e, data){
-        res.send(data)
-      });
-});
+router.get('/', controller.search);
+router.get('/trends', controller.search);
 
 module.exports = router;
